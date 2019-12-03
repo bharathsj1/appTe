6d74e7a802fc4aad80b271b60c8c806664ac844f
Bharath <bharath.sj1@gmail.com>
2019-12-03 21:45:20 +0530

    Initial Commit

---
+++ appTe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  appTe
+//
+//  Created by Bharath on 03/12/19.
+//  Copyright © 2019 Bharath. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
